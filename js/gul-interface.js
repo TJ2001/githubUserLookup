0715fce1969c3922fd3b222f14ab1d63b09aab4a
TJ2001 <jung.m.tim@Gmail.com>
2016-08-05 15:00:18 -0700

    got api to call repo information properly

--- js/gul-interface.js
+++ js/gul-interface.js
@@ -1,19 +1,26 @@
 var User = require("./../js/gul.js").userModule;
+var apiKey = require("./../.env").apiKey;
 
 var gitUser = new User();
 
 var displayName = function(name) {
   $(".showName").html("Name: " + name);
-}
+};
+
+// $(document).ready(function() {
+//   $("form#githubUser").submit(function() {
+//     event.preventDefault();
+//     var username = $("#userName").val();
+//     gitUser.getName(username);
+//   });
+// });
+
 
 $(document).ready(function() {
   $("form#githubUser").submit(function() {
     event.preventDefault();
     var username = $("#userName").val();
-    gitUser.getName(username);
-    $(".repoURL").html("URL for Repository: " + "<a href=" + response.repos_url + ">" + response.repos_url + "</a>");
-    $.get("https://api.github.com/users/" + username + "/repos").then(function(repos){
-      console.log(JSON.stringify(repos));
-    });
+    gitUser.getInfo(username);
+    // .then(gitUser.getRepo(username));
   });
 });
--- js/gul.js
+++ js/gul.js
@@ -1,16 +1,22 @@
 var apiKey = require("./../.env").apiKey;
 
 function User() {
-  this.answer;
 }
 
-User.prototype.getName = function(username) {
+User.prototype.getInfo = function(username) {
   $.get("https://api.github.com/users/" + username + "?access_token=" + apiKey).then(function(response) {
-  console.log(JSON.stringify(response));
-  console.log(response.repos_url);
-  $(".showName").html("Name: " + response.name);
-  $(".publicRepos").html("Number of Public Repos: " + response.public_repos);
+    $(".showName").html("Name: " + response.name);
+    $(".publicRepos").html("Number of Public Repos: " + response.public_repos);
+    var repoQuantity = response.public_repos;
+    $.get("https://api.github.com/users/" + username + "/repos").then(function(repos){
+      for (var i = 0; i < repoQuantity; i++) {
+        console.log(repoQuantity);
+        console.log(repos[i].name);
+      }
+    });
   });
 };
+
+
 
 exports.userModule = User;
