25a7375be7ab20a87687b765133c18775f75acd4
TJ2001 <jung.m.tim@gmail.com>
2016-08-05 09:13:19 -0700

    fixed error in gulpfile.js

---
+++ js/gul-interface.js
@@ -0,0 +1,13 @@
+var apiKey = require('./../.env').apiKey;
+// var gul = require('./../js/gul.js').telephoneModule;
+// var ourTelephone = new Telephone()
+
+$(document).ready(function() {
+  // $('#lookup').click(function() {
+  //   $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+  //     console.log(response);
+  //   }).fail(function(error){
+  //     console.log(error.responseJSON.message);
+  //   });
+  // };
+});
