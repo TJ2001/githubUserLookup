226c3f5d6202200f1a6b356c2f5bbd25f3640b03
TJ2001 <jung.m.tim@gmail.com>
2016-08-12 15:51:29 -0700

    add script in user.js to clearlist before repopulation

--- js/user-interface.js
+++ js/user-interface.js
@@ -1,4 +1,4 @@
-var User = require("./../js/gul.js").userModule;
+var User = require("./../js/user.js").userModule;
 var apiKey = require("./../.env").apiKey;
 
 var gitUser = new User();
@@ -7,20 +7,10 @@ var displayName = function(name) {
   $(".showName").html("Name: " + name);
 };
 
-// $(document).ready(function() {
-//   $("form#githubUser").submit(function() {
-//     event.preventDefault();
-//     var username = $("#userName").val();
-//     gitUser.getName(username);
-//   });
-// });
-
-
 $(document).ready(function() {
   $("form#githubUser").submit(function() {
     event.preventDefault();
     var username = $("#userName").val();
     gitUser.getInfo(username, displayName);
-    // .then(gitUser.getRepo(username));
   });
 });
--- js/user.js
+++ js/user.js
@@ -7,13 +7,14 @@ function User() {
 
 User.prototype.getInfo = function(username, displayFunction) {
   $.get("https://api.github.com/users/" + username + "?access_token=" + apiKey).then(function(response) {
+    $("ul").empty();
     displayFunction(username, response.name);
     // $(".showName").html("Name: " + response.name);
     var actualNumber = response.public_repos - 1;
     $(".publicRepos").html("Number of Public Repos: " + actualNumber);
     var repoQuantity = response.public_repos;
     $.get("https://api.github.com/users/" + username + "/repos?page=1&per_page=" + repoQuantity).then(function(repos){
-      console.log(repoQuantity)
+      console.log(repoQuantity);
       for (var i = 1; i < repoQuantity; i++) {
         console.log(i);
         $(".repos").append("<li>" + i + ". " + repos[i].name);
